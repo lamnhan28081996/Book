97dedf0e8f958e366ba06ff718801e0167658031
nhan680780 <lamnhan28081996@gmail.com>
2018-05-24 17:21:19 +0700

    Làm tới task G2

--- depot/app/controllers/line_items_controller.rb
+++ depot/app/controllers/line_items_controller.rb
@@ -33,9 +33,10 @@ class LineItemsController < ApplicationController
     respond_to do |format|
       if @line_item.save
 
-        format.html { redirect_to @line_item.cart,
-              notice: 'Line item was successfully created.'}
-        format.json { render :show, status: :created, location: @line_item }
+        format.html { redirect_to store_index_url }
+        format.json { @current_item = @line_item }
+        format.json { render :show,
+          status: :created, location: @line_item }
       else
         format.html { render :new }
         format.json { render json: @line_item.errors, status: :unprocessable_entity }
@@ -75,6 +76,6 @@ class LineItemsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def line_item_params
-      params.require(:line_item).permit(:product_id, :cart_id)
+      params.require(:line_item).permit(:product_id)
     end
 end
