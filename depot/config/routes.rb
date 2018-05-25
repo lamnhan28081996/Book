24521671ccb92d843286caa8cb282757afb4c739
nhan680780 <lamnhan28081996@gmail.com>
2018-05-25 15:31:51 +0700

    Làm tới phần Task J

--- depot/config/routes.rb
+++ depot/config/routes.rb
@@ -1,10 +1,28 @@
 Rails.application.routes.draw do
-  resources :orders
-  resources :line_items
-  resources :carts
+  get 'admin' => 'admin#index'
+  controller :sessions do
+    get 'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
+  get 'admin/index'
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
+  resources :users
+  # resources :orders
+  # resources :line_items
+  # resources :carts
   # get 'store/index'
-  root 'store#index', as: 'store_index'
-  resources :products
-
+  # root 'store#index', as: 'store_index'
+  resources :products do
+		get :who_bought, on: :member
+  end
+  scope '(:locale)' do
+    resources :orders
+    resources :line_items
+    resources :carts
+    root 'store#index', as: 'store_index', via: :all
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
